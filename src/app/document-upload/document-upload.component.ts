6c6850b7e396f8a5fbdb2613792c40a7d09f605d
zyw271828 <zyw_0618@outlook.com>
2021-06-11 15:27:45 +0800

    Add formGroup and validator

--- src/app/document-upload/document-upload.component.ts
+++ src/app/document-upload/document-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-document-upload',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-upload.component.css']
 })
 export class DocumentUploadComponent implements OnInit {
+  documentUploadForm = this.fb.group({
+    resourceType: [null, Validators.required],
+    documentType: [null, Validators.required],
+    name: [null, Validators.required],
+    isNamePublic: false,
+    entityAssetID: null,
+    precedingDocumentID: null,
+    headDocumentID: null,
+    content: [null, Validators.required],
+    policy: [null, Validators.required]
+  });
+
   resourceTypes: String[] = [
     'Plaintext',
     'Encryption (on chain)',
@@ -22,12 +35,28 @@ export class DocumentUploadComponent implements OnInit {
 
   filename: String = "";
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  checkPrecedingDocumentID() {
+    // TODO: checkPrecedingDocumentID, change color instead of pop-up prompt
+    alert('Preceding Document ID checked');
+  }
+
+  checkHeadDocumentID() {
+    // TODO: checkHeadDocumentID, change color instead of pop-up prompt
+    alert('Head Document ID checked');
+  }
+
   fileInputChange(fileInputEvent: any) {
     this.filename = fileInputEvent.target.files[0].name;
+  }
+
+  onSubmit(): void {
+    // TODO: submit documentUploadForm
+    // TODO: use MatDialog
+    alert('Upload successfully');
   }
 }
