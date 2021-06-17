613793f80b7504890d34864bb3e2848fb6e2fc98
zyw271828 <zyw_0618@outlook.com>
2021-06-17 10:20:35 +0800

    Update auth-request

--- src/app/auth-request/auth-request.component.ts
+++ src/app/auth-request/auth-request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-request',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-request.component.css']
 })
 export class AuthRequestComponent implements OnInit {
+  authRequestForm = this.fb.group({
+    resourceID: [null, Validators.required],
+    reason: null
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    if (this.authRequestForm.valid) {
+      // TODO: submit authRequestForm
+      // this.dialog.open(AuthRequestPromptDialog, {
+      //   data: {
+      //     title: 'Result',
+      //     content: 'Request successfully',
+      //     action: 'Close'
+      //   }
+      // });
+    }
+  }
 }
