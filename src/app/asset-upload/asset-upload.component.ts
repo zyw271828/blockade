4f4bf1e809d2988698a0dcd62abec3dcf8b06171
zyw271828 <zyw_0618@outlook.com>
2021-06-18 10:12:58 +0800

    Update asset-upload

--- src/app/asset-upload/asset-upload.component.ts
+++ src/app/asset-upload/asset-upload.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Utils } from '../utils';
 
 @Component({
   selector: 'app-asset-upload',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asset-upload.component.css']
 })
 export class AssetUploadComponent implements OnInit {
+  assetUploadForm = this.fb.group({
+    resourceType: [null, Validators.required],
+    name: [null, Validators.required],
+    componentIDs: [null, Validators.required],
+    policy: [null, Validators.required]
+  });
 
-  constructor() { }
+  resourceTypes: String[] = Utils.getResourceTypes('asset');
+
+  currentResourceType: String = "";
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  resourceTypeChange(currentResourceType: String) {
+    if (currentResourceType === this.resourceTypes[0]) {
+      this.assetUploadForm.get("policy")?.disable();
+    } else {
+      this.assetUploadForm.get("policy")?.enable();
+    }
+  }
+
+  assetUploadResetButtonClick() {
+    this.assetUploadForm.get("policy")?.enable();
+    this.assetUploadForm.reset();
+  }
+
+  onSubmit(): void {
+    if (this.assetUploadForm.valid) {
+      // TODO: submit assetUploadForm
+    } else { // assetUploadForm is invalid
+    }
+  }
 }
