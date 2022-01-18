ef28b3e84ecb21b6e7f4ed829562c35e88474da2
zyw271828 <zyw_0618@outlook.com>
2022-01-18 22:13:32 +0800

    Update the logic for querying and downloading assets

--- src/app/asset-query-result-table/asset-query-result-table.component.ts
+++ src/app/asset-query-result-table/asset-query-result-table.component.ts
@@ -6,6 +6,7 @@ import { MatTable } from '@angular/material/table';
 import { AssetQueryComponent } from '../asset-query/asset-query.component';
 import { AssetService } from '../asset.service';
 import { AuthDialogComponent } from '../auth-dialog/auth-dialog.component';
+import { Utils } from '../utils';
 import { AssetQueryResultTableDataSource, AssetQueryResultTableItem } from './asset-query-result-table-datasource';
 
 export interface DialogData {
@@ -36,6 +37,8 @@ export class AssetQueryResultTableComponent implements AfterViewInit {
     'operation'
   ];
 
+  mask: string = Utils.mask;
+
   constructor(private assetQueryComponent: AssetQueryComponent, private assetService: AssetService, public dialog: MatDialog) {
     this.dataSource = new AssetQueryResultTableDataSource(this.assetQueryComponent, this.assetService);
   }
@@ -47,51 +50,41 @@ export class AssetQueryResultTableComponent implements AfterViewInit {
   }
 
   showDetail(row: AssetQueryResultTableItem) {
-    let authDialog = this.dialog.open(AuthDialogComponent, {
-      width: '350px',
-      data: {
-        title: 'Authentication',
-        resourceID: row.resourceID
-      }
-    });
+    if (row.name === undefined
+      || row.designDocumentID === undefined) {
+      row.resourceType = Utils.getRawResourceType('asset', row.resourceType);
 
-    authDialog.afterClosed().subscribe(() => {
-      if (authDialog.componentInstance.keySwitchSessionID !== undefined) {
-        this.dialog.open(AssetQueryResultDetailDialog, {
-          data: {
-            title: 'Detail',
-            content: [
-              { item: 'ResourceID', value: row.resourceID },
-              { item: 'Name', value: row.name },
-              { item: 'ResourceType', value: row.resourceType },
-              { item: 'Hash', value: row.hash },
-              { item: 'CiphertextHash', value: row.ciphertextHash },
-              { item: 'Size', value: row.size },
-              { item: 'CiphertextSize', value: row.ciphertextSize },
-              { item: 'Creator', value: row.creator },
-              { item: 'CreationTime', value: row.creationTime },
-              { item: 'DesignDocumentID', value: row.designDocumentID }
-            ]
-          }
-        });
-      } else { // authDialog.componentInstance.keySwitchSessionID is undefined
-        this.dialog.open(AssetQueryResultDetailDialog, {
-          data: {
-            title: 'Detail',
-            content: [
-              { item: 'ResourceID', value: row.resourceID },
-              { item: 'Name', value: row.name },
-              { item: 'ResourceType', value: row.resourceType },
-              { item: 'Hash', value: row.hash },
-              { item: 'CiphertextHash', value: row.ciphertextHash },
-              { item: 'Size', value: row.size },
-              { item: 'CiphertextSize', value: row.ciphertextSize },
-              { item: 'Creator', value: row.creator },
-              { item: 'CreationTime', value: row.creationTime },
-              { item: 'DesignDocumentID', value: row.designDocumentID }
-            ]
-          }
-        });
+      this.assetService.getAssetById(row.resourceID, row.resourceType).subscribe((asset) => {
+        if (row.name === undefined) {
+          row.name = asset.name;
+        }
+        if (row.designDocumentID === undefined) {
+          row.designDocumentID = asset.designDocumentID;
+        }
+      });
+
+      this.openDetailDialog(row);
+    } else { // row.name, row.designDocumentID are not undefined
+      this.openDetailDialog(row);
+    }
+  }
+
+  openDetailDialog(row: AssetQueryResultTableItem) {
+    this.dialog.open(AssetQueryResultDetailDialog, {
+      data: {
+        title: 'Detail',
+        content: [
+          { item: 'ResourceID', value: row.resourceID },
+          { item: 'Name', value: row.name === undefined ? this.mask : row.name },
+          { item: 'ResourceType', value: row.resourceType },
+          { item: 'Hash', value: row.hash },
+          { item: 'CiphertextHash', value: row.ciphertextHash },
+          { item: 'Size', value: row.size },
+          { item: 'CiphertextSize', value: row.ciphertextSize },
+          { item: 'Creator', value: row.creator },
+          { item: 'CreationTime', value: row.creationTime },
+          { item: 'DesignDocumentID', value: row.designDocumentID === undefined ? this.mask : row.designDocumentID }
+        ]
       }
     });
   }
@@ -108,9 +101,29 @@ export class AssetQueryResultDetailDialog {
     'value'
   ];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private assetService: AssetService) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private assetService: AssetService, public dialog: MatDialog) { }
+
+  decryptAsset(resourceID: string, resourceType: string) {
+    let authDialog = this.dialog.open(AuthDialogComponent, {
+      width: '350px',
+      data: {
+        title: 'Authentication',
+        resourceID: resourceID
+      }
+    });
+
+    authDialog.afterClosed().subscribe(() => {
+      resourceType = Utils.getRawResourceType('asset', resourceType);
+
+      this.assetService.getAssetById(resourceID, resourceType, authDialog.componentInstance.keySwitchSessionID).subscribe((asset) => {
+        // TODO: replace mask and open detail dialog
+      });
+    });
+  }
 
   downloadAsset(resourceID: string, resourceType: string, keySwitchSessionID?: string) {
+    resourceType = Utils.getRawResourceType('asset', resourceType);
+
     this.assetService.getAssetById(resourceID, resourceType, keySwitchSessionID).subscribe(asset => {
       let file = new File([], asset.name); // TODO: download asset by resourceID
       let link = self.document.createElement('a');
