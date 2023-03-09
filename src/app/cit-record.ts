27da0533ac6507fbd581dc01f79b7a36460c42c6
zyw271828 <zyw_0618@outlook.com>
2023-03-09 11:12:16 +0800

    Add cit-record interface

---
+++ src/app/cit-record.ts
@@ -0,0 +1,8 @@
+// https://resources.sei.cmu.edu/library/asset-view.cfm?assetid=508099
+export interface CitRecord {
+  id: string; // "{Y6O4-A7KC67IN-0899AOZK}"
+  date: string; // "01/04/2010 00:10:37"
+  user: string; // "DTAA/KEE0997"
+  pc: string; // "PC-1914"
+  activity: string; // "Logon" || "Logoff" || "Connect" || "Disconnect" || "http://cnet.com"
+}
