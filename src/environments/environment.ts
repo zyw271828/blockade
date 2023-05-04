0672da60b0358b747e1717c2ac5bff8b57e00941
zyw271828 <zyw_0618@outlook.com>
2023-05-04 10:48:48 +0800

    Update apiEndpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const apiEndpoints: string[] = [
   "http://localhost:8082/api/v1",
   "http://localhost:8083/api/v1",
   "http://localhost:8084/api/v1",
-  "http://localhost:8085/api/v1"
+  "http://localhost:8085/api/v1",
+  "http://localhost:8086/api/v1"
 ];
 
 export let environment = {
